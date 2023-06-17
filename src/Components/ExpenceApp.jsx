92ce4d3d79723757698cb01f59c96f3f4d9f644c
immortal <ali.hatampouri@gmail.com>
2023-06-17 21:18:02 +0330

    init project

---
+++ src/Components/ExpenceApp.jsx
@@ -0,0 +1,23 @@
+const ExpenceApp = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Expence manager</h1>
+      <div>
+        <span>balance: 5</span>
+        <button>Add</button>
+      </div>
+      <div>
+        <div>
+          <strong>Expence</strong>
+          <span>1$</span>
+        </div>
+        <div>
+          <strong>Income</strong>
+          <span>1$</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ExpenceApp;
